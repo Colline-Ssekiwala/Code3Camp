8cd88d67325f92915157ac49033130268deee812
Ssekiwala-Colline <ssekiwal8@gmail.com>
2024-06-20 13:16:53 +0300

    Removed the url

--- Code3CampProject/Code3CampProject/urls.py
+++ Code3CampProject/Code3CampProject/urls.py
@@ -21,5 +21,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('app1/', include('app1.urls')),
     path('app2/', include('app2.urls')),
-    path('app5/', include('app2.urls')),
+    
 ]
